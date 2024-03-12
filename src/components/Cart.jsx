9a7918b756771afedeee029a250d84fd3eced408
Krishna Mahto <130828781+Krishnamahto07@users.noreply.github.com>
2024-03-13 01:11:02 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,11 +9,11 @@ function Cart(){
         setTotalAmount( cart.reduce( (acc,curr) => acc+curr.price,0));
     },[cart])
     return (
-        <div>
+        <div className="flex items-center justify-center w-full">
             {
                 cart.length > 0 ?
                 (
-                    <div>
+                    <div className="w-[50%] p-2  min-h-[80vh] flex ">
                         <div>
                             {
                                 cart.map((item,index) =>{
@@ -21,26 +21,31 @@ function Cart(){
                                 })
                             }
                         </div>
-                        <div>
-                            <div>
-                                <h2>Your Cart</h2>
-                                <h4>Summary</h4>
-                                <p>
-                                    <span>Total Items :{cart.length} </span>
-                                </p>
+                        <div className="w-6/12 ml-8 ">
+                            <div className="w-full flex flex-col items-center gap-5 mx-auto">
+                                <h2 className="text-gray-700 font-semibold text-4xl text-left truncate w-40 mt-1">Your Cart</h2>
+                                <h4 className="text-blue-400 font-bold text-xl ">Summary</h4>
+                                
+                                <div className="flex gap-4 mt-2 flex-col">
+                            
+                                    <span className="text-2xl ">Total Items :  {cart.length} </span>
+                                    <p className="text-md text-lg font-semibold">Amount : <span className="text-green-800 font-bold text-2xl">${totalAmount}</span></p>
+                                    <button className="bg-green-200 px-3 py-2 rounded-md font-semibold hover:text-green-900 transition duration-300 hover:border" >Checkout Now</button>
+                            
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
                 ):(
-                    <div>
-                    <h1>Cart Empty</h1>
-                    <Link to={"/"}>
-                    <button>Shop Now</button>
-                    </Link>
-                    <div>
-                        <p>Total Amount : ${totalAmount}</p>
-                        <button>Checkout Now</button>
-                    </div>
+                    <div className="w-full h-full flex gap-4 items-center justify-center">
+                        <div className="">
+                        <h1 className="text-4xl font-bold text-red-900 p-2">Cart Empty</h1>
+                        <Link to={"/"}>
+                        <button className="bg-green-200 px-3 py-2 rounded-md font-semibold hover:text-green-900 transition duration-300 hover:border">Shop Now</button>
+                        </Link>
+                        
+                        </div>
                     </div>
                 )
             }
