252fa3f5f762fa9bf0415e0298dd00633a1aa62a
Krishna Mahto <krishnamahato84044@gmail.com>
2024-03-22 14:17:31 +0530

    second-commite

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ function Home(){
     return (
         <div>
         {
-          loading ? <Spinner />  :
+          loading ? <div className="w-full h-[100vh] flex justify-center items-center"><Spinner /></div>  :
           posts.length > 0 ? 
           (<div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
             {
